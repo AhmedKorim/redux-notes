d10c37f0c912b198da71f03f593afaa541937cda
ahmedkorim <ahmedkorrim@gmail.com>
2018-08-11 17:01:58 +0300

    simple dispatcher
    @counter.js @reducer.js

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -2,6 +2,12 @@ const intialState = {
     counter: 0
 }
 const reducer = (state = intialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
     return state;
 }
 export default reducer;
